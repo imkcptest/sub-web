cda6a5aed8c3092610045311d4f0f69f60e09e64
imkcptest <97370793+imkcptest@users.noreply.github.com>
2022-07-16 14:19:11 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -255,7 +255,7 @@ export default {
           ClashR: "clashr",
           Surge2: "surge&ver=2",
         },
-        backendOptions: [{ value: "https://imkcpblogtest.herokuapp.com?" }],
+        backendOptions: [{ value: "https://subconver-imkcproxy.koyeb.app/sub?" }],
         remoteConfig: [
           {
             label: "universal",
